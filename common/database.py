e40044f63e88189c70e598b6fe1394e8bb6366b4
Michael Nash <michael@spatialnetworks.com>
2019-08-15 15:16:07 -0400

    whoops

--- common/database.py
+++ common/database.py
@@ -4,7 +4,7 @@ import pymongo
 
 class Database(object):
   URI = os.environ.get("MONGODB_URI")
-  DATABASE = pymongo.MongoClient(URI).get_default_database()
+  DATABASE = pymongo.MongoClient(URI).get_database()
 
   @staticmethod
   def insert(collection: str, data: Dict):
